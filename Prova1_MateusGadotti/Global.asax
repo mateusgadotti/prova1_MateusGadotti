7477e6b789a02f147b9e18fcf9337dd8389647b9
mateusgadotti <mateus.gadotti@catolicasc.org.br>
2017-09-26 19:11:14 -0300

    Add project files.

---
+++ Prova1_MateusGadotti/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Prova1_MateusGadotti.MvcApplication" Language="C#" %>
---
+++ Prova1_MateusGadotti/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Prova1_MateusGadotti.Startup))]
+namespace Prova1_MateusGadotti
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
