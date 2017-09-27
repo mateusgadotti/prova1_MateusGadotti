52b2c1f79ec49a6b4f33d1982231602415dcd3b0
mateusgadotti <mateus.gadotti@catolicasc.org.br>
2017-09-26 22:00:20 -0300

    add2

--- Prova1_MateusGadotti/Migrations/201709270025010_Migration3.cs
+++ Prova1_MateusGadotti/Migrations/201709270025010_Migration3.cs
@@ -8,10 +8,10 @@ namespace Prova1_MateusGadotti.Migrations
         public override void Up()
         {
             
-            Sql("INSERT INTO Alunoes(Faixa_Id, TipoDeAssociacao_Id, Nome, CPF) VALUES (1, 21 , 'Martim Castro Ribeiro', '158.778.735-04')");
-            Sql("INSERT INTO Alunoes(Faixa_Id, TipoDeAssociacao_Id, Nome, CPF) VALUES (4, 22 , 'Lucas Alves Melo', '916.749.332-74')");
-            Sql("INSERT INTO Alunoes(Faixa_Id, TipoDeAssociacao_Id, Nome, CPF) VALUES (2, 23 , 'Breno Castro Carvalho', '454.019.800-94')");
-            Sql("INSERT INTO Alunoes(Faixa_Id, TipoDeAssociacao_Id, Nome, CPF) VALUES (6, 24 , 'Laura Cardoso Rocha', '361.089.665-56')");
+            Sql("INSERT INTO Alunoes(Faixa_Id, TipoDeAssociacao_Id, Nome, CPF) VALUES (1, 1 , 'Martim Castro Ribeiro', '158.778.735-04')");
+            Sql("INSERT INTO Alunoes(Faixa_Id, TipoDeAssociacao_Id, Nome, CPF) VALUES (4, 3 , 'Lucas Alves Melo', '916.749.332-74')");
+            Sql("INSERT INTO Alunoes(Faixa_Id, TipoDeAssociacao_Id, Nome, CPF) VALUES (2, 2 , 'Breno Castro Carvalho', '454.019.800-94')");
+            Sql("INSERT INTO Alunoes(Faixa_Id, TipoDeAssociacao_Id, Nome, CPF) VALUES (6, 4 , 'Laura Cardoso Rocha', '361.089.665-56')");
         }
 
         public override void Down()
